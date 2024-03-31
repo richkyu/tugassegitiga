f71b3a756d89562db38d046354c2ee0cacf2c552
richkyu <kadekaryana92@gmail.com>
2024-03-31 21:38:16 +0800

    first commit

---
+++ Documents/NetBeansProjects/tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,44 @@
+package tugas1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+/**
+ *
+ * @author Prince Kouu
+ * TGL : 2024 - 03 - 28
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BufferedReader dtain = new BufferedReader (new InputStreamReader (System.in));
+        
+        Double luas;
+        int al = 0;
+        int ti = 0;
+        String alas ="";
+        String tinggi="";
+        
+        System.out.println("Hitung Luas Segitiga");
+        
+        try{
+            System.out.print("Input Nilai Alas: ");
+            alas = dtain.readLine();
+            System.out.print("Input Nilai Tinggi: ");
+            tinggi = dtain.readLine();
+        }catch(IOException e) {
+            System.out.println("ERROR!");
+        }
+        al = Integer.parseInt(alas);
+        ti = Integer.parseInt(tinggi);
+        
+        // Proses hitungannya
+        luas = 0.5 * al * ti;
+       
+        //outputnya
+        System.out.println("Hasil Luas Segitiga: " + luas);
+    }
+    
+}
